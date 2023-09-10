56ea150988578b14942fc6bb01e2586da014f34f
Pradeep Khandale <136149076+SoloMonarch@users.noreply.github.com>
2023-09-10 21:51:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+    var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var images1 = "images/dice"+ randomNumber1 +".png";
+var images2 = "images/dice"+ randomNumber2 +".png";
+
+ document.querySelector(".left").setAttribute("src",images1);
+ document.querySelector(".right").setAttribute("src",images2);
+
+
+
+    if (randomNumber1 === randomNumber2){
+        document.querySelector("h4").innerHTML = "Draw";
+        document.querySelector(".winner").setAttribute("src","images/cat.jpg");
+    }
+    else if (randomNumber1 >= randomNumber2){
+        document.querySelector("h4").innerHTML = "Coline Win";
+        document.querySelector(".winner").setAttribute("src","images/coline.jpg");
+    }
+    else if (randomNumber1 <= randomNumber2){
+        document.querySelector("h4").innerHTML = "Pradeep Win";
+        document.querySelector(".winner").setAttribute("src","images/pradeep.jpg");
+    }
